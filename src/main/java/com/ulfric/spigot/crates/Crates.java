699d9fa517aaa78b9d2be7858c8a8893cd5f2263
Luke <development@inkzzz.com>
2017-05-14 20:13:11 +0100

    initial

---
+++ src/main/java/com/ulfric/spigot/crates/Crates.java
@@ -0,0 +1,13 @@
+package com.ulfric.spigot.crates;
+
+import com.ulfric.commons.spigot.plugin.UlfricPlugin;
+
+public final class Crates extends UlfricPlugin {
+	
+	@Override
+	public void init()
+	{
+		
+	}
+	
+}
